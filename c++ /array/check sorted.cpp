ef5e2e4f783e4f7c543daa3e24d5886919c1c0a0
Pradeepa.G <pradeepa03.in@gmail.com>
2026-08-11 19:07:32 +0530

    Create check sorted.cpp

---
+++ c++ /array/check sorted.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    
+    int arr[n];
+    
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    
+    bool sorted=true;
+    
+    for(int i=0;i<n;i++){
+        if(arr[i]>arr[i+1]){
+            sorted=false;
+            break;
+        }
+    }
+    
+    if(sorted){
+        cout<<"Element is sorted";
+    }
+    else{
+        cout<<"Element is not found";
+    }
+}
