f708e1b17d24c8b679e27227f458b6ee3148c1a3
Pradeepa.G <pradeepa03.in@gmail.com>
2026-08-20 12:14:10 +0530

    Create search element.cpp

---
+++ C++/2D-array/search element.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,m;
+    cin>>n>>m;
+    
+    int arr[n][m];
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>arr[i][j];
+        }
+    }
+    
+    bool found=false;
+    int target;
+    cin>>target;
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(arr[i][j]==target){
+                found=true;
+                break;
+            }
+        }
+        if(found){
+            break;
+        }
+    }
+    
+    if(found){
+        cout<<"element is found";
+    }
+    else{
+        cout<<"Element is not found";
+    }
+}
