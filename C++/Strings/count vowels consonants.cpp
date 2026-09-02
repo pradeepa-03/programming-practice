d7cce0fd833623a8ec87d944bd66b2d1804219ca
Pradeepa.G <pradeepa03.in@gmail.com>
2026-09-02 09:27:36 +0530

    Create count vowels consonants.cpp

---
+++ C++/Strings/count vowels consonants.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string str;
+    getline(cin,str);
+    
+    int vowels=0;
+    int consonants=0;
+    
+    for(int i=0;i<str.length();i++){
+        char ch=str[i];
+        
+        if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
+            vowels++;
+        }
+        else if(ch>='a'&&ch<='z'||ch>='A'&&ch<='Z'){
+            consonants++;
+        }
+    }
+    
+    cout<<"Vowels count: "<<vowels<<endl;
+    cout<<"Consonants count: "<<consonants;
+}
